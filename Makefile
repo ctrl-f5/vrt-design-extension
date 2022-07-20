126cd0ac932411c55ea2f6f102dd8b2ad0f41d67
Nicky De Maeyer <nicky.demaeyer@gmail.com>
2022-07-20 12:22:06 +0200

    initial chrome extension

---
+++ Makefile
@@ -0,0 +1,4 @@
+.PHONY: build
+build:
+	mkdir -p build
+	zip build/vrt-design-extension.zip manifest.json content-script.js style.css icon-128.png icon-192.png
---
+++ content-script.js
@@ -0,0 +1,13 @@
+$(() => {
+    $('#main-content .vrtnws-sb-container > div > section:first-of-type > ul')
+        .each((key, block) => {
+            let list = $(block).find('ul:first-of-type');
+            if ($(list).find('> li').length > 2) {
+                $(block).find('ul:first-of-type').next('div').remove();
+                let copy = $(block).clone();
+                copy.insertAfter(block);
+                $(list).find('> li:not(:first-child)').remove();
+                $(copy).find('ul:first-of-type > li:first-child').remove();
+            }
+        })
+});
