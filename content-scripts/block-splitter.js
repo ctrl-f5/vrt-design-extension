711642249df7122c3f6f934a3524da6762acda34
Nicky De Maeyer <nicky.demaeyer@gmail.com>
2022-07-20 23:32:43 +0200

    media queries to switch between 2, 3, or 4 columns depending on the width. Also add border to cloned blocks

--- content-scripts/block-splitter.js
+++ content-scripts/block-splitter.js
@@ -5,6 +5,7 @@ $(() => {
             if ($(list).find('> li').length > 2) {
                 $(block).find('ul:first-of-type').next('div').remove();
                 let copy = $(block).clone();
+                copy.addClass('big-block-clone');
                 copy.insertAfter(block);
                 $(list).find('> li:not(:first-child)').remove();
                 $(copy).find('ul:first-of-type > li:first-child').remove();
